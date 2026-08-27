dcdda0230921c07b25cecc18902d02e9899aa9d9
sssantass <servce7@gmail.com>
2026-08-27 14:16:01 +0700

    simple num guesser

--- challenge1.py
+++ challenge1.py
@@ -1,1 +1,11 @@
 # test
+import random
+num = random.randint(1, 3)
+print(num)
+while True:
+    choice = int(input("Predict the number from 1 to 3: "))
+    if choice == num:
+        print("You guessed it right!")
+        break
+    else:
+        print("Wrong")
